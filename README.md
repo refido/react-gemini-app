e0662826a16371ebde5c1db9f94a48a2d7536920
revido <revidoberliano@gmail.com>
2024-02-24 00:35:10 +0700

    Simple chat features

--- README.md
+++ README.md
@@ -1,10 +1,20 @@
-# Getting Started with Create React App
+# Getting Started
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+This project is a simple web application that allows users to chat with an AI bot powered by Gemini's NLP API. The frontend is built using React and the backend is built using Node.js and Express.
 
 ## Available Scripts
 
 In the project directory, you can run:
+
+### `npm run start:frontend`
+
+Runs the frontend server in the development mode.
+
+### `npm run start:backend`
+
+Runs the backend server in the development mode.
 
 ### `npm start`
 
@@ -13,11 +23,6 @@ Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
 
 The page will reload when you make changes.\
 You may also see any lint errors in the console.
-
-### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
 
 ### `npm run build`
 
@@ -64,7 +69,3 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/a
 ### Deployment
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState("");
+  const [error, setError] = useState("");
+  const [chatHistory, setChatHistory] = useState([]);
+
+  const surpriseOptions = [
+    "When is Sunday?",
+    "What is the capital of France?",
+    "What is the weather in London?",
+    "How many people live in New York?",
+    "What is the population of China?",
+  ];
+
+  const surprise = () => {
+    const randomValue = surpriseOptions[Math.floor(Math.random() * surpriseOptions.length)];
+    setValue(randomValue);
+  }
+
+  const getResponse = async () => {
+    if (!value) {
+      setError("Please enter a question!");
+      return;
+    }
+    try {
+      const options = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          history: chatHistory,
+          message: value
+        }),
+      };
+      const response = await fetch("http://localhost:8000/gemini", options);
+      const data = await response.text();
+      console.log(data);
+      setChatHistory(oldChatHistory => [...oldChatHistory,
+      { role: "user", parts: value },
+      { role: "model", parts: data }
+      ]);
+      setValue("");
+    } catch (error) {
+      console.error(error);
+      setError("There was an error fetching the data.");
+    }
+  }
+
+  const clear = () => {
+    setValue("");
+    setError("");
+    setChatHistory([]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <p>Hi there! What do you want to know?
+        <button className="surprise" onClick={surprise} disabled={!chatHistory}>Surprise me!</button>
+      </p>
+      <div className="input-container">
+        <input value={value} onChange={(e) => setValue(e.target.value)} type="text" placeholder="When is Sunday...?" />
+        {!error && <button onClick={getResponse}>Ask me!</button>}
+        {error && <button onClick={clear}>Clear</button>}
+      </div>
+      {error && <p>{error}</p>}
+      <div className="search-results">
+        {chatHistory.map((chatItem, _index) =>
+          <div key={_index}>
+            <p className="answer">{chatItem.role} : {chatItem.parts}</p>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
